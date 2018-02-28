5abc49a2bc233fee3d828d4052c6b9db178d9e9b
Igor Voropaev <snakework10@gmail.com>
2018-02-28 15:39:35 +0500

    Fixed webhook

--- webapp/views.py
+++ webapp/views.py
@@ -14,8 +14,11 @@ class FormView(MethodView):
 class WebhookDialogflow(MethodView):
     def post(self):
         data = request.get_json(silent=True, force=True)
+        print(data)
         if data.get("result").get("action") != "check.duty":
             return make_response(jsonify(self.check_duty(data)))
+        
+        return make_response()
 
     def get_duty(account):
         return -1000000000
