2572f228853694a6669e729eb80c358df8632b65
Igor Snake <snakeego@users.noreply.github.com>
2018-04-10 16:30:51 +0500

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -54,7 +54,7 @@ class WebhookDialogflow(MethodView):
     def get_readings(self, account, fio):
         try:
             with urllib.request.urlopen(
-                    f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(bytes(fio, 'utf-8'))}') as response:
+                    f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(bytes(fio, "utf-8"))}') as response:
                 debt = json.loads(response.read())
 
             return (f"Адрес: {debt['address']}",
