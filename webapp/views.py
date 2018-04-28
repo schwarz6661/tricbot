c5d6be25cf33b017cdf90292bf5524eab61410a5
Igor Voropaev <snakework10@gmail.com>
2018-04-28 17:38:32 +0500

    Refactored views methods

--- webapp/views.py
+++ webapp/views.py
@@ -2,6 +2,13 @@ import urllib
 from flask.views import MethodView
 from flask import render_template, request, jsonify, make_response, json
 
+SHORTCODE = {
+    'Холодное водоснабжение': 'ХВ',
+    'Горячее водоснабжение': 'ГВ',
+    'Электроэнергия (день)': 'ЭЭ (день)',
+    'Электроэнергия (ночь)': 'ЭЭ (ночь)'
+}
+
 
 class APIQueryError(Exception):
     pass
@@ -18,6 +25,22 @@ class FormView(MethodView):
         return render_template("form.html", text='Hey')
 
 
+def api_query(fn):
+    def query(*args, **kwargs):
+        try:
+            fn(*args, **kwargs)
+        except urllib.request.HTTPError as err:
+            if err.code in (500, 503):
+                raise APIQueryError("Сервер недоступен")
+            elif err.code == 404:
+                raise APIQueryError("Неправильный лицевой счет")
+            elif err.code == 408:
+                raise APIQueryError("Нет ответа от сервера")
+            else:
+                raise APIQueryError("Что-то пошло не так")
+    return query
+
+
 class WebhookDialogflow(MethodView):
     def post(self):
         data = request.get_json(silent=True, force=True)
@@ -32,92 +55,6 @@ class WebhookDialogflow(MethodView):
             return make_response(jsonify(self.verification(data)))
 
         return make_response()
-
-    def get_duty(self, account):
-        try:
-            with urllib.request.urlopen(f'https://api.itpc.ru/v1/accounts/{account}/debt', timeout=1) as response:
-                debt = json.loads(response.read())
-
-            return (f"По вашему лицевому счету: {account}",
-                    f"Адрес: {debt['address']}",
-                    f"Ваша задолженность: {debt['amount']}")
-        except urllib.request.HTTPError as err:
-            if err.code == 500:
-                raise APIQueryError("Сервер недоступен")
-            elif err.code == 503:
-                raise APIQueryError("Сервер недоступен")
-            elif err.code == 404:
-                raise APIQueryError("Неправильный лицевой счет")
-            elif err.code == 408:
-                raise APIQueryError("Нет ответа от сервера")
-            else:
-                raise APIQueryError("Что-то пошло не так")
-
-
-    def get_readings(self, account, fio):
-        try:
-            with urllib.request.urlopen(
-                    f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(fio)}') as response:
-                counters = json.loads(response.read())
-            counters_print = []
-            shortcode = {
-                'Холодное водоснабжение': 'ХВ',
-                'Горячее водоснабжение': 'ГВ',
-                'Электроэнергия (день)': 'ЭЭ (день)',
-                'Электроэнергия (ночь)': 'ЭЭ (ночь)'
-            }
-
-            for i in counters['counters']:
-                if i['place'] is None or i['model'] is None:
-                    counters_print.append('{place}: {name}. {counter}'.format(name=shortcode.get(i['name'], i['name']),
-                                                                              place='Место не указано ',
-                                                                              counter=i['currReadings']))
-                else:
-                    counters_print.append('{place}: {model}. {name}. {counter}'.format(name=shortcode.get(i['name'], i['name']),
-                                                                                       place=i['place'],
-                                                                                       model=i['model'],
-                                                                                       counter=i['currReadings']))
-            return (f"Адрес: {counters['address']}", "Показания:") + tuple(counters_print)
-
-        except urllib.request.HTTPError as err:
-            if err.code == 500:
-                raise APIQueryError("Сервер недоступен")
-            elif err.code == 404:
-                raise APIQueryError("Неправильный лицевой счет")
-            else:
-                raise APIQueryError("Что-то пошло не так")
-
-
-    def get_verify(self, account, fio):
-        try:
-            with urllib.request.urlopen(
-                    f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(fio)}') as response:
-                counters = json.loads(response.read())
-                counters_print = []
-                shortcode = {
-                    'Холодное водоснабжение': 'ХВ',
-                    'Горячее водоснабжение': 'ГВ',
-                    'Электроэнергия (день)': 'ЭЭ (день)',
-                    'Электроэнергия (ночь)': 'ЭЭ (ночь)'
-                }
-            for i in counters['counters']:
-                if i['place'] is None or i['model'] is None or i['nextVerificationRemaining'] < 0:
-                    counters_print.append('Комм. услуга {name}. {nextVerificationMessage}!'.format(nextVerificationMessage=i['nextVerificationMessage'],
-                                                                                                name=shortcode.get(i['name'])))
-                else:
-                    counters_print.append('Счетчик модели {model} комм. услуги {name}. ({place}) До следующей поверки {nextVerificationRemaining} дн.'.format(place=i['place'],
-                                                                                                model=i['model'],
-                                                                                                name=shortcode.get(i['name']),
-                                                                                                nextVerificationRemaining=i['nextVerificationRemaining']))
-            return (f"Адрес: {counters['address']}", "Счетчики:") + tuple(counters_print)
-
-        except urllib.request.HTTPError as err:
-            if err.code == 500:
-                raise APIQueryError("Сервер недоступен")
-            elif err.code == 404:
-                raise APIQueryError("Неправильный лицевой счет")
-            else:
-                raise APIQueryError("Что-то пошло не так")
 
     def verification(self, data):
         account = int(data.get("queryResult", dict()).get("parameters", dict()).get("account"))
@@ -148,3 +85,46 @@ class WebhookDialogflow(MethodView):
         except APIQueryError as e:
             speech = str(e)
         return {'payload': {'telegram': {"text": speech}}, "source": "tricbot"}
+
+    @api_query
+    def get_duty(self, account):
+        with urllib.request.urlopen(f'https://api.itpc.ru/v1/accounts/{account}/debt', timeout=1) as response:
+            debt = json.loads(response.read())
+
+        return (f"По вашему лицевому счету: {account}",
+                f"Адрес: {debt['address']}",
+                f"Ваша задолженность: {debt['amount']}")
+
+    @api_query
+    def get_readings(self, account, fio):
+        with urllib.request.urlopen(
+                f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(fio)}') as response:
+            counters = json.loads(response.read())
+        counters_print = []
+
+        for i in counters['counters']:
+            if i['place'] is None or i['model'] is None:
+                counters_print.append('{place}: {name}. {counter}'.format(name=SHORTCODE.get(i['name'], i['name']),
+                                                                          place='Место не указано ',
+                                                                          counter=i['currReadings']))
+            else:
+                counters_print.append('{place}: {model}. {name}. {counter}'.format(name=SHORTCODE.get(i['name'], i['name']),
+                                                                                   place=i['place'],
+                                                                                   model=i['model'],
+                                                                                   counter=i['currReadings']))
+        return (f"Адрес: {counters['address']}", "Показания:") + tuple(counters_print)
+
+    @api_query
+    def get_verify(self, account, fio):
+        with urllib.request.urlopen(
+                f'https://api.itpc.ru/v1/accounts/{account}/counters?lastname={urllib.parse.quote(fio)}') as response:
+            counters = json.loads(response.read())
+            counters_print = []
+
+        for i in counters['counters']:
+            if i.get('place') is None or i['model'] is None or i['nextVerificationRemaining'] < 0:
+                counters_print.append(f"Комм. услуга {SHORTCODE.get(i['name'])}. {i['nextVerificationMessage']}!")
+            else:
+                counters_print.append(f"Счетчик модели {i['model']} комм. услуги {SHORTCODE.get(i['name'])}. "
+                                      f"({i['place']}) До следующей поверки {i['nextVerificationRemaining']} дн.")
+        return (f"Адрес: {counters['address']}", "Счетчики:") + tuple(counters_print)
