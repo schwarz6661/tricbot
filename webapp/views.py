45b19eae2799915b7c83c2138962582da08b00de
schwarz6661 <36918225+schwarz6661@users.noreply.github.com>
2018-05-25 17:42:13 +0500

    Update views.py

--- webapp/views.py
+++ webapp/views.py
@@ -106,7 +106,7 @@ class WebhookDialogflow(MethodView):
         counters_print = []
         k=0
         for i in counters['counters']:
-            if counters['counters'] is None:
+            if counters['counters'] == ' ':
                 counters_print.append(f"Счетчики отсутствуют!")
             if i['place'] is None or i['model'] is None:
                 counters_print.append(f"{k}. Место не указано: {SHORTCODE.get(i['name'])}. {i['currReadings']}")
